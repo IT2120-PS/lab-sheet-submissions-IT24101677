18557594851365ffb3ad173d1cae9bbe6f1ab690
IT24101677 <it24101677@my.sliit.lk>
2025-09-02 14:08:18 +0530

    Lab6

---
+++ Lab Sheet 06/Lab6.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\IT24101677\\Desktop\\Lab6")
+getwd()
+
+#1
+#i)Binomial Distribution
+#ii)
+1-pbinom(46,50,0.85, lower.tail = TRUE)
+
+#2
+#i Number of calls per hour
+#ii Poison Distribution
+#iii
+dpois(15,12)
